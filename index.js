fbaa0d717aa0b698ad4965458cdefc1d7e02a1d9
mallikarjun2000 <arjunmallik093@gmail.com>
2020-07-18 20:00:55 +0530

    Project working fine

---
+++ index.js
@@ -0,0 +1,22 @@
+var btn = document.getElementById('submit');
+btn.style.background ="#1234abc"
+document.write('<h1>Hello World</h1>')
+function check(){
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    if(email.indexOf('@') == -1)
+    {
+        alert(' Enter correct email ')
+    }
+    else
+    {
+        if(password.length < 9)
+        {
+            alert( ' Password you entered might not be strong');
+        }
+        else
+        {
+            alert(' Accepted ')
+        }
+    }
+}
